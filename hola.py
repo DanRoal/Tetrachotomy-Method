512d9a08d397ea376b6a59ed7f0a73831d3ba099
Dan <danroal@ciencias.unam.mx>
2023-10-16 22:04:48 -0600

    Initial commit

---
+++ hola.py
@@ -0,0 +1,7 @@
+"""
+Primer acercacimiento al intentar hacer un programa que haga el método de tetracotomía para funciones de
+variable compleja.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
