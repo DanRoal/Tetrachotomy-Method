c257fb81c0faab0d3259eced98f42e5b52145660
Dan <danroal@ciencias.unam.mx>
2023-10-22 04:21:25 -0600

    Segundo intento de integracion la integrla se hace desde 0 a 2Pi falla, y se tarda un chingo más

--- main.py
+++ main.py
@@ -12,11 +12,18 @@ import sympy as sp
 #definimos el cambio de variable
 x_0, x_1 = 0, 1
 y_0, y_1 = 0, 1
-def P(x):
-    return x**3 *(6*x**2 - 15*x + 10)
 
-def delta_P(x):
-    return 30*(x-1)**2 * x**2
+t = sp.symbols('t')
+x = sp.symbols('x')
+def P(x, a, b):
+    numerador = sp.integrate((t-a)**2 * (b-t)**2, (t, a, x))
+    denominador = sp.integrate((t-a)**2 * (b-t)**2, (t, a, b))
+    return numerador/denominador
+
+def delta_P(argumento, a, b):
+    delta = sp.diff(P(x, a, b), x)
+    funcion =  sp.lambdify(x, delta)
+    return funcion(argumento)
 
 def gamma_down(t):
     return complex((x_1 - x_0) * t + x_0, y_0)
@@ -32,31 +39,37 @@ def gamma_left(t):
 
 #definimos la función que queremos integrar
 def f(z):
-    return 1/((0.5 + 0.5j)-z)
+    return z/(z - (0.1 + 0.4j))
+def f_1(z):
+    return 1/(z - (0.1 + 0.4j))
 
-N=50
+N=100
 h = (1)/N
 
-"""
-int_down_0 = sum([delta_P(i/N) * f(gamma_down(P(i/N)))* 1/(P(i/N)) for i in range(N)]) * h
-int_right_0 = sum([delta_P(i/N) * f(gamma_right(P(i/N)))* 1/(P(i/N)) for i in range(N)]) * h
-int_up_0 = sum([delta_P(i/N) * f(gamma_up(P(i/N)))* 1/(P(i/N)) for i in range(N)]) * h
-int_left_0 = sum([delta_P(i/N) * f(gamma_left(P(i/N)))* 1/(P(i/N)) for i in range(N)]) * h
 
-inttegral_0 = int_down_0 + int_right_0 + int_up_0 + int_left_0
-"""
-int_down_1 = sum([delta_P(i/N) * f(gamma_down(P(i/N))) for i in range(N)]) * h
-int_right_1 = sum([delta_P(i/N) * f(gamma_right(P(i/N))) for i in range(N)]) * h
-int_up_1 = sum([delta_P(i/N) * f(gamma_up(P(i/N))) for i in range(N)]) * h
-int_left_1 = sum([delta_P(i/N) * f(gamma_left(P(i/N))) for i in range(N)]) * h
+
+int_down_0 = sum([delta_P(i/N, 0, sp.pi/2) * f_1(gamma_down(P(i/N, 0, sp.pi/2))) for i in range(N)]) * h
+int_right_0 = sum([delta_P(i/N, 0, sp.pi/2) * f_1(gamma_right(P(i/N, 0, sp.pi/2))) for i in range(N)]) * h
+int_up_0 = sum([delta_P(i/N, 0, sp.pi/2) * f_1(gamma_up(P(i/N, 0, sp.pi/2))) for i in range(N)]) * h
+int_left_0 = sum([delta_P(i/N, 0, sp.pi/2) * f_1(gamma_left(P(i/N, 0, sp.pi/2))) for i in range(N)]) * h
+
+integral_0 = int_down_0 + int_right_0 + int_up_0 + int_left_0
+
+int_down_1 = sum([delta_P(i/N, sp.pi/2,sp.pi) * f(gamma_down(P(i/N,sp.pi/2 , sp.pi))) for i in range(N)]) * h
+int_right_1 = sum([delta_P(i/N, sp.pi/2,sp.pi) * f(gamma_right(P(i/N,sp.pi/2 , sp.pi))) for i in range(N)]) * h
+int_up_1 = sum([delta_P(i/N, sp.pi/2,sp.pi) * f(gamma_up(P(i/N,sp.pi/2 , sp.pi))) for i in range(N)]) * h
+int_left_1 = sum([delta_P(i/N, sp.pi/2,sp.pi) * f(gamma_left(P(i/N,sp.pi/2 , sp.pi))) for i in range(N)]) * h
 
 integral_1 = int_down_1 + int_right_1 + int_up_1 + int_left_1
-
+"""
 int_down_2 = sum([delta_P(i/N) * f(gamma_down(P(i/N)))* P(i/N) for i in range(N)]) * h
 int_right_2 = sum([delta_P(i/N) * f(gamma_right(P(i/N)))* P(i/N) for i in range(N)]) * h
 int_up_2 = sum([delta_P(i/N) * f(gamma_up(P(i/N)))* P(i/N) for i in range(N)]) * h
 int_left_2 = sum([delta_P(i/N) * f(gamma_left(P(i/N)))* P(i/N) for i in range(N)]) * h
 
 integral_2 = int_down_2 + int_right_2 + int_up_2 + int_left_2
+"""
 
-print(integral_1, integral_2)
+print(integral_0)
+print(integral_1)
+print(integral_1/integral_0)
